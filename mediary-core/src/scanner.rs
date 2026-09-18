64b5a56f51406e8a482b8fcd7cf8c62a925a2014
Gavin Borne <gavin.bor0925@gmail.com>
2026-09-18 10:44:07 -0400

    add base error and struct for scans

---
+++ mediary-core/src/scanner.rs
@@ -0,0 +1,72 @@
+//! # scanner.rs
+//!
+//! Functionality for file-scanning in mediary.
+//!
+//! Copyright (C) 2026  Gavin Borne
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+    io,
+    path::PathBuf,
+};
+
+use crate::{data::database::SqliteInsertError, types::Media};
+
+/// An error that may occur during a file/directory scan.
+///
+/// # Variants
+///
+/// - `Io(io::Error)` - An I/O error.
+/// - `Insert(SqliteInsertError)` - A database insert error.
+#[derive(Debug)]
+pub enum ScanError {
+    Io(io::Error),
+    Insert(SqliteInsertError),
+}
+impl Display for ScanError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            ScanError::Io(e) => write!(f, "{}", e),
+            ScanError::Insert(e) => write!(f, "{}", e),
+        }
+    }
+}
+impl Error for ScanError {}
+impl From<io::Error> for ScanError {
+    fn from(e: io::Error) -> Self {
+        ScanError::Io(e)
+    }
+}
+impl From<SqliteInsertError> for ScanError {
+    fn from(e: SqliteInsertError) -> Self {
+        ScanError::Insert(e)
+    }
+}
+
+/// The result of a file/directory scan.
+///
+/// # Fields
+///
+/// - `inserted` (`Vec<Media>`) - The media items that were inserted.
+/// - `skipped_duplicates` (`Vec<PathBuf>`) - The files that were skipped.
+/// - `errors` (`Vec<(PathBuf, ScanError)>`) - The files that had errors.
+#[derive(Debug)]
+pub struct ScanResult {
+    pub inserted: Vec<Media>,
+    pub skipped_duplicates: Vec<PathBuf>,
+    pub errors: Vec<(PathBuf, ScanError)>,
+}
